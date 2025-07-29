09c04b1a82b36dd02710867603df4606b35151ce
João <57973684+WhaleJucs@users.noreply.github.com>
2025-07-29 10:26:12 -0300

    Lógica para fim de jogo

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,8 @@ const stages = [
   {id: 3, name: 'end'}
 ]
 
+const guessesQty = 3
+
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
@@ -27,7 +29,7 @@ function App() {
   const [letters, setLetters] = useState([])
   const [guessedLetters, setGuessedLetters] = useState([])
   const [wrongLetters, setWrongLetters] = useState([])
-  const [guesses, setGuesses] = useState(3)
+  const [guesses, setGuesses] = useState(guessesQty)
   const [score, setScore] = useState(0)
 
   const pickWordAndCategory = () => {
@@ -88,14 +90,32 @@ function App() {
         ...actualWrongLetters,
         nomalizedLetter
       ])
+
+      setGuesses((actualGuesses) => actualGuesses - 1)
     }
   }
+
+  const clearLetterStates = () => {
+    setGuessedLetters([])
+    setWrongLetters([])
+  }
+
+  useEffect(() => {
+    if(guesses <= 0) {
+      // reset all states
+      clearLetterStates()
+
+      setGameStage(stages[2].name)
+    }
+  }, [guesses])
 
   console.log(guessedLetters)
   console.log(wrongLetters)
 
   // restarts the game
   const retry = () => {
+    setScore(0)
+    setGuesses(guessesQty)
     setGameStage(stages[0].name)
   }
 
